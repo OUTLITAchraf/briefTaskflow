fdbcb414bc141618ce98c72034bba3495bc53352
OutlitAchraf <outlitachraf5@gmail.com>
2025-09-08 15:00:03 +0100

    install laratrust

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -12,7 +13,8 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                        <h2 className='p-6 text-2xl font-bold text-gray-900'>Bienvenue {auth.user.name}</h2>
+                        <p className="p-6 text-gray-900">You're logged in!</p>
                     </div>
                 </div>
             </div>
